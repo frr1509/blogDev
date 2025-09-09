e99d1d1be6cd208531b69cb4479f3bff91e1083e
Alexsey Sinyakin <lexavandal@mail.ru>
2025-09-09 21:52:22 +0300

    fix fin

--- backend/app.js
+++ backend/app.js
@@ -5,19 +5,27 @@ import express from "express";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 import routers from "./routes/index.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const port = 3001;
 const app = express();
 
-app.use(express.static("../frontend/build"));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.use(express.json());
 app.use(cookieParser());
 
+
 app.use("/api", routers);
 
+
 app.use((req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
 });
 
 mongoose.connect(process.env.MONGO_CONNECTION_STRING).then(() => {
